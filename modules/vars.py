dea01ed111a9de57e40ae0e34bd68f0086c4940a
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-03-19 01:36:24 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -4,7 +4,7 @@ import os
 
 API_ID    = os.environ.get("API_ID", "26537149")
 API_HASH  = os.environ.get("API_HASH", "2697a63a384fd9b95520ee63530f6a75")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7568454294:AAGAwmx-hL_ArwgfIE-ZLlmT3mJbVYSu-uo") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 BILLA = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
