5ca634e72a08f3c03d80fc8b7578102f0567983e
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-03-19 01:18:35 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
-# SudoR2spr WOODcraft
+# SudoR2spr BILLA
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
@@ -6,6 +6,6 @@ API_ID    = os.environ.get("API_ID", "26537149")
 API_HASH  = os.environ.get("API_HASH", "2697a63a384fd9b95520ee63530f6a75")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7568454294:AAGAwmx-hL_ArwgfIE-ZLlmT3mJbVYSu-uo") 
 
-WEBHOOK = True  # Don't change this
+BILLA = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
 
